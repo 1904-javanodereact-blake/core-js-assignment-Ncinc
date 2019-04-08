58a5e4669a64ea6c0fcfb7c85c70c2fc66cbb05b
Ncinc <nicholascordon1@gmail.com>
2019-04-08 00:14:18 -0400

    update

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -3,4 +3,19 @@ Define function: fib(n)
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
 
+    if(n===1)
+    {
+        return 1;
+    }
+    if(n===2)
+    {
+        return 1;
+    }
+    else 
+    {
+        return fib(n-1) + fib(n-2)
+    }
+
 }
+console.log(fib(6));
+
