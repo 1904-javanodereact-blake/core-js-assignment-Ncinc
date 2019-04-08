cebb82aa9e9acc6d591e037167335e4123823369
Ncinc <nicholascordon1@gmail.com>
2019-04-08 09:51:16 -0400

    update

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,16 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
+
+let boat = ['Chair', 'Cabin', 'Rudder', 'Wheel'];
 function objectProperties(someObj) {
 
+    for (let objectfield in someObj)
+    {
+        console.log(`objectfield = ${objectfield} 
+        and value = ${someObj [objectfield]}`);
+    }
+return someObj;
 }
+
+console.log(objectProperties(boat))
