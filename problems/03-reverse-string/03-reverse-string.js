a1373a13041028ee11b1ec533fafbc48e6a92540
Ncinc <nicholascordon1@gmail.com>
2019-04-08 08:26:45 -0400

    update

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,28 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
-function reverseStr(someStr) {
-  
-}
+
+ function reverseStr(someStr){
+     let changed = "";    
+     for (let i = someStr.length -1; i >= 0; i--){   
+         changed += someStr[i];
+      // console.log(changed)
+      // console.log(someStr.length)
+    }    
+    return changed;
+ }
+   console.log(reverseStr('hello'))
+
+
+  function inorderStr2(someStr){
+    let changed2 = "";    
+    let position = 1;
+    for (let i = 0; i <= someStr.length - 1; i++){   
+        changed2 += someStr[i];
+      //console.log(changed)
+      //console.log(someStr.length)
+    }    
+    return changed2;
+    console.log(changed2)
+  }
+  console.log(inorderStr2('hello'))
